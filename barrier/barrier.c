a35346369c751c726bda760940b9fc5bad8ac468
garbled <garbled>
2004-10-04 18:20:33 +0000

    Convert to autoconf, ansification.

--- barrier/barrier.c
+++ barrier/barrier.c
@@ -1,4 +1,4 @@
-/* $Id: barrier.c,v 1.14 2003/11/02 15:33:07 garbled Exp $ */
+/* $Id: barrier.c,v 1.15 2004/10/04 18:20:33 garbled Exp $ */
 /*
  * Copyright (c) 1998, 1999, 2000
  *	Tim Rightnour.  All rights reserved.
@@ -47,151 +47,145 @@
 __COPYRIGHT(
 "@(#) Copyright (c) 1998, 1999, 2000\n\
         Tim Rightnour.  All rights reserved\n");
-__RCSID("$Id: barrier.c,v 1.14 2003/11/02 15:33:07 garbled Exp $");
+__RCSID("$Id: barrier.c,v 1.15 2004/10/04 18:20:33 garbled Exp $");
 #endif
 
 int quietflag, barrier_port, debug;
 char *barrier_host;
 char *progname;
 
-#ifndef __P
-#define __P(protos) protos
-#endif
-
-int make_barrier __P((char *key, int nodes));
-int write_to_server __P((int filedes, char *buf));
-void log_bailout __P((int));
+int make_barrier(char *key, int nodes);
+int write_to_server(int filedes, char *buf);
+void _log_bailout(int line, char *file);
 
 int
-main(int argc, char *argv[])
+main(int argc, char **argv)
 {
-	extern char *optarg;
+    extern char *optarg;
 
-	char *key;
-	int nodes, ch, code;
+    char *key;
+    int nodes, ch, code;
 	
-	barrier_port = BARRIER_SOCK;
-	barrier_host = NULL;
-	quietflag = 1;
-	key = NULL;
-	nodes = 0;
+    barrier_port = BARRIER_SOCK;
+    barrier_host = NULL;
+    quietflag = 1;
+    key = NULL;
+    nodes = 0;
 
 #if defined(__linux__)
-	while ((ch = getopt(argc, argv, "+?dh:k:p:s:")) != -1)
+    while ((ch = getopt(argc, argv, "+?dh:k:p:s:")) != -1)
 #else
 	while ((ch = getopt(argc, argv, "?dh:k:p:s:")) != -1)
 #endif
-		switch (ch) {
-		case 'q':
-			quietflag = 0;
-			break;
-		case 'h':
-			barrier_host = strdup(optarg);
-			break;
-		case 'k':
-			key = strdup(optarg);
-			break;
-		case 'p':
-			barrier_port = atoi(optarg);
-			break;
-		case 's':
-			nodes = atoi(optarg);
-			break;
-		case '?':
-			(void)fprintf(stderr, "usage: barrier [-q] [-h barrier_host]"
-			    " [-k key] [-p port] -s cluster_size\n");
-			exit(EXIT_FAILURE);
-			break;
-		default:
-			break;
-	}
-	if (nodes < 1) {
-		(void)fprintf(stderr, "usage: barrier [-q] [-h barrier_host] "
-			"[-k key] [-p port] -s cluster_size\n");
+	    switch (ch) {
+	    case 'q':
+		quietflag = 0;
+		break;
+	    case 'h':
+		barrier_host = strdup(optarg);
+		break;
+	    case 'k':
+		key = strdup(optarg);
+		break;
+	    case 'p':
+		barrier_port = atoi(optarg);
+		break;
+	    case 's':
+		nodes = atoi(optarg);
+		break;
+	    case '?':
+		(void)fprintf(stderr, "usage: barrier [-q] [-h barrier_host]"
+		    " [-k key] [-p port] -s cluster_size\n");
 		exit(EXIT_FAILURE);
+		break;
+	    default:
+		break;
+	    }
+    if (nodes < 1) {
+	(void)fprintf(stderr, "usage: barrier [-q] [-h barrier_host] "
+	    "[-k key] [-p port] -s cluster_size\n");
+	exit(EXIT_FAILURE);
+    }
+    if (key == NULL)
+	key = strdup("barrier");
+    if (key == NULL)
+	bailout();
+
+    if (barrier_port == BARRIER_SOCK)
+	if (getenv("BARRIER_PORT") != NULL)
+	    barrier_port = atoi(getenv("BARRIER_PORT"));
+
+    if (barrier_host == NULL) {
+	if (getenv("BARRIER_HOST") != NULL)
+	    barrier_host = strdup(getenv("BARRIER_HOST"));
+	else {
+	    (void)fprintf(stderr, "No barrier host given on command line,"
+		" and BARRIER_HOST environment not found.\n");
+	    exit(EXIT_FAILURE);
 	}
-	if (key == NULL)
-		key = strdup("barrier");
-	if (key == NULL)
-		bailout(__LINE__);
+    }
 
-	if (barrier_port == BARRIER_SOCK)
-		if (getenv("BARRIER_PORT") != NULL)
-			barrier_port = atoi(getenv("BARRIER_PORT"));
-
-	if (barrier_host == NULL) {
-		if (getenv("BARRIER_HOST") != NULL)
-			barrier_host = strdup(getenv("BARRIER_HOST"));
-		else {
-			(void)fprintf(stderr, "No barrier host given on command line,"
-				" and BARRIER_HOST environment not found.\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	code = make_barrier(key, nodes);
-	if (code == 6) {
-		if (!quietflag)
-			(void)printf("Barrier met, continuing: %s\n", key);
-		return(EXIT_SUCCESS);
-	} else
-		if (!quietflag)
-			(void)printf("Barrier encountered a server error, aborting.\n");
-	return(EXIT_FAILURE); 
+    code = make_barrier(key, nodes);
+    if (code == 6) {
+	if (!quietflag)
+	    (void)printf("Barrier met, continuing: %s\n", key);
+	return(EXIT_SUCCESS);
+    } else
+	if (!quietflag)
+	    (void)printf("Barrier encountered a server error, aborting.\n");
+    return(EXIT_FAILURE); 
 }
 
-int make_barrier(key, nodes)
-	char *key;
-	int nodes;
+int make_barrier(char *key, int nodes)
 {
-	int sock;
-	char *p;
-	char message[256];
-	struct sockaddr_in name;
-	struct hostent *hostinfo;
+    int sock;
+    char *p;
+    char message[256];
+    struct sockaddr_in name;
+    struct hostent *hostinfo;
 
-	/* create socket */
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+    /* create socket */
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+	perror("socket");
+	exit(EXIT_FAILURE);
+    }
 
-	name.sin_family = AF_INET;
-	name.sin_port = htons(barrier_port);
-	hostinfo = gethostbyname(barrier_host);
+    name.sin_family = AF_INET;
+    name.sin_port = htons(barrier_port);
+    hostinfo = gethostbyname(barrier_host);
 
-	if (hostinfo == NULL) {
-		(void)fprintf(stderr, "Unknown host %s.\n", barrier_host);
-		exit(EXIT_FAILURE);
-	}
+    if (hostinfo == NULL) {
+	(void)fprintf(stderr, "Unknown host %s.\n", barrier_host);
+	exit(EXIT_FAILURE);
+    }
 
-	name.sin_addr = *(struct in_addr *) hostinfo->h_addr;
+    name.sin_addr = *(struct in_addr *) hostinfo->h_addr;
 
-	if (connect (sock, (struct sockaddr *) &name, sizeof (name)) < 0) {
-		perror ("connect");
-		exit (EXIT_FAILURE);
-	}
-	if (!quietflag)
-		(void)printf("Barrier syncing with token: %s\n", key);
-	(void)sprintf(message, "%s %d", key, nodes);
-	write_to_server(sock, message);
-	return(read(sock, &p, 6));
+    if (connect (sock, (struct sockaddr *) &name, sizeof (name)) < 0) {
+	perror ("connect");
+	exit (EXIT_FAILURE);
+    }
+    if (!quietflag)
+	(void)printf("Barrier syncing with token: %s\n", key);
+    (void)sprintf(message, "%s %d", key, nodes);
+    write_to_server(sock, message);
+    return(read(sock, &p, 6));
 }
 
 int write_to_server(int filedes, char *buf)
 {
-	int nbytes;
+    int nbytes;
 
-	nbytes = write(filedes, buf, strlen(buf));
-	if (nbytes < 0) {
-		return(EXIT_FAILURE);
-	}
-	return(0);
+    nbytes = write(filedes, buf, strlen(buf));
+    if (nbytes < 0) {
+	return(EXIT_FAILURE);
+    }
+    return(0);
 }
 
 void
-log_bailout(line)
+_log_bailout(int line, char *file)
 {
-	bailout(line);
+    _bailout(line, file);
 }
--- barrier/barrierd.c
+++ barrier/barrierd.c
@@ -1,4 +1,4 @@
-/* $Id: barrierd.c,v 1.14 2003/11/02 15:33:07 garbled Exp $ */
+/* $Id: barrierd.c,v 1.15 2004/10/04 18:20:33 garbled Exp $ */
 /*
  * Copyright (c) 1998, 1999, 2000
  *	Tim Rightnour.  All rights reserved.
@@ -45,7 +45,7 @@
 __COPYRIGHT(
 "@(#) Copyright (c) 1998, 1999, 2000\n\
         Tim Rightnour.  All rights reserved\n");
-__RCSID("$Id: barrierd.c,v 1.14 2003/11/02 15:33:07 garbled Exp $");
+__RCSID("$Id: barrierd.c,v 1.15 2004/10/04 18:20:33 garbled Exp $");
 #endif
 
 #define MAX_TOKENS	10
@@ -54,192 +54,184 @@ __RCSID("$Id: barrierd.c,v 1.14 2003/11/02 15:33:07 garbled Exp $");
 int barrier_port, debug;
 char *progname;
 
-#ifndef __P
-#define __P(protos) protos
-#endif
-
-int sleeper __P((void));
-void log_bailout __P((int));
+int sleeper(void);
+void _log_bailout(int line, char *file);
 
 #if !defined(__NetBSD__) && !defined(__linux__)
 char * strsep(char **stringp, const char *delim);
 #endif
 
 int
-main(int argc, char *argv[])
+main(int argc, char **argv)
 {
-	extern char *optarg;
+    extern char *optarg;
 
-	int ch;
+    int ch;
 	
-	barrier_port = 0;
+    barrier_port = 0;
 
 #if defined(__linux__)
-	while ((ch = getopt(argc, argv, "+?p:")) != -1)
+    while ((ch = getopt(argc, argv, "+?p:")) != -1)
 #else
 	while ((ch = getopt(argc, argv, "?p:")) != -1)
 #endif
-		switch (ch) {
-		case 'p':
-			barrier_port = atoi(optarg);
-			break;
-		case '?':
-			(void)fprintf(stderr, "usage: barrierd [-p port]\n");
-			exit(EXIT_FAILURE);
-			break;
-		default:
-			break;
-	}
+	    switch (ch) {
+	    case 'p':
+		barrier_port = atoi(optarg);
+		break;
+	    case '?':
+		(void)fprintf(stderr, "usage: barrierd [-p port]\n");
+		exit(EXIT_FAILURE);
+		break;
+	    default:
+		break;
+	    }
 
-	if (barrier_port == 0) {
-		if (getenv("BARRIER_PORT") != NULL)
-			barrier_port = atoi(getenv("BARRIER_PORT"));
-		else
-			barrier_port = BARRIER_SOCK;
-	}
-	return(sleeper());
+    if (barrier_port == 0) {
+	if (getenv("BARRIER_PORT") != NULL)
+	    barrier_port = atoi(getenv("BARRIER_PORT"));
+	else
+	    barrier_port = BARRIER_SOCK;
+    }
+    return(sleeper());
 }
 
 int
 sleeper(void)
 {
-	fd_set active_fd_set, read_fd_set;
-	struct sockaddr_in clientname;
-	char *key, *buf;
-	char *tokens[MAX_TOKENS];
-	int i, k, l, m, found, sock;
-	size_t size;
-	int sizes[MAX_TOKENS];
-	int connections[MAX_TOKENS];
-	int sockets[MAX_TOKENS][MAX_CLUSTER];
+    fd_set active_fd_set, read_fd_set;
+    struct sockaddr_in clientname;
+    char *key, *buf;
+    char *tokens[MAX_TOKENS];
+    int i, k, l, m, found, sock;
+    size_t size;
+    int sizes[MAX_TOKENS];
+    int connections[MAX_TOKENS];
+    int sockets[MAX_TOKENS][MAX_CLUSTER];
 
 #ifndef DEBUG
-	switch (fork()) {
-	case 0: 
+    switch (fork()) {
+    case 0: 
 #endif
-		/* Create the socket and set it up to accept connections. */
-		sock = make_socket(barrier_port);
+	/* Create the socket and set it up to accept connections. */
+	sock = make_socket(barrier_port);
 
-		if (listen(sock, 1) < 0)
-			log_bailout(__LINE__);
+	if (listen(sock, 1) < 0)
+	    log_bailout();
 
-		for (l=0; l < MAX_TOKENS; l++) {
-			connections[l] = 0;
-			tokens[l] = NULL;
-			for (m=0; m < MAX_CLUSTER; m++)
-				sockets[l][m] = 0;
-		}	
+	for (l=0; l < MAX_TOKENS; l++) {
+	    connections[l] = 0;
+	    tokens[l] = NULL;
+	    for (m=0; m < MAX_CLUSTER; m++)
+		sockets[l][m] = 0;
+	}	
 
-		/* Initialize the set of active sockets. */
-		FD_ZERO(&active_fd_set);
-		FD_SET(sock, &active_fd_set);
+	/* Initialize the set of active sockets. */
+	FD_ZERO(&active_fd_set);
+	FD_SET(sock, &active_fd_set);
 #ifdef DEBUG
-		printf("Server going into wait mode, listening on port %d\n",
-			barrier_port);
+	printf("Server going into wait mode, listening on port %d\n",
+	    barrier_port);
 #endif
-		while (1) {
-			/* Block until input arrives on one or more active sockets. */
-			read_fd_set = active_fd_set;
-			if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
-				log_bailout(__LINE__);
+	while (1) {
+	    /* Block until input arrives on one or more active sockets. */
+	    read_fd_set = active_fd_set;
+	    if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
+		log_bailout();
 
-			/* Service all the sockets with input pending. */
-			for (i = 0; i < FD_SETSIZE; ++i)
-				if (FD_ISSET (i, &read_fd_set)) {
-					if (i == sock) {
-						/* Connection request on original socket. */
-						int new;
-						size = sizeof(clientname);
-						new = accept(sock, (struct sockaddr *) &clientname,
-							&size);
-						if (new < 0)
-							log_bailout(__LINE__);
+	    /* Service all the sockets with input pending. */
+	    for (i = 0; i < FD_SETSIZE; ++i)
+		if (FD_ISSET (i, &read_fd_set)) {
+		    if (i == sock) {
+			/* Connection request on original socket. */
+			int new;
+			size = sizeof(clientname);
+			new = accept(sock, (struct sockaddr *) &clientname,
+			    &size);
+			if (new < 0)
+			    log_bailout();
 #ifdef DEBUG
-						(void)fprintf(stderr,
-							"Server: connect from host %s, port %hd.\n",
-						    inet_ntoa(clientname.sin_addr),
-							ntohs(clientname.sin_port));
+			(void)fprintf(stderr,
+			    "Server: connect from host %s, port %hd.\n",
+			    inet_ntoa(clientname.sin_addr),
+			    ntohs(clientname.sin_port));
 #endif
-						FD_SET(new, &active_fd_set);
-						if (read_from_client(new, &buf) > 0) {
-							key = (char *)strsep(&buf, " ");
-							found = 0;
-							for (k=0; (found == 0 && k < MAX_TOKENS); ) {
-								if (tokens[k] != NULL)
-									if (strcmp(tokens[k], key) == 0)
-										found = 1;
-								if (!found)
-									k++;
-							}
-							if (!found) /* we didn't find a matching token,
-										   now make a new one */
-							for (k=0; (k < MAX_TOKENS && tokens[k] != NULL);
-								 k++);
+			FD_SET(new, &active_fd_set);
+			if (read_from_client(new, &buf) > 0) {
+			    key = (char *)strsep(&buf, " ");
+			    found = 0;
+			    for (k=0; (found == 0 && k < MAX_TOKENS); ) {
+				if (tokens[k] != NULL)
+				    if (strcmp(tokens[k], key) == 0)
+					found = 1;
+				if (!found)
+				    k++;
+			    }
+			    if (!found) /* we didn't find a matching token,
+					   now make a new one */
+				for (k=0; (k < MAX_TOKENS && tokens[k] != NULL);
+				     k++);
 
-							if (k > MAX_TOKENS - 1) {
-								(void)fprintf(stderr, "Server: too many "
-									"tokens. Disconnected host %s, "
-									"port %hd.\n",
-									inet_ntoa(clientname.sin_addr),
-									ntohs(clientname.sin_port));
-								write_to_client(new, "fail");
-								close(new);
-								FD_CLR(new, &active_fd_set);
-							} else { 
-								for (l=0; sockets[k][l] != 0; l++)
-									;
-								if (l > MAX_CLUSTER - 1) {
-									(void)fprintf(stderr, "Server: too many "
-										"sockets on token. Disconnected "
-										"host %s, port %hd.\n",
-									    inet_ntoa(clientname.sin_addr),
-										ntohs(clientname.sin_port));
-									write_to_client(new, "fail");
-									close(new);
-									FD_CLR(new, &active_fd_set);
-								} else {
-									sockets[k][l] = new;
-									tokens[k] = key;
-									sizes[k] = atoi(buf);
-									connections[k] += 1;
-									if (connections[k] == sizes[k]) {
-										l = connections[k];
-										for (m=0; m < l; m++) {
-											write_to_client(sockets[k][m],
-												"passed");
-											close(sockets[k][m]);
-											FD_CLR (sockets[k][m], 
-												&active_fd_set);
-											sockets[k][m] = 0;
-											tokens[k] = NULL;
-											sizes[k] = 0;
-											connections[k] = 0;
-										} /* for */
-									} /* connections == sizes */
-								} /* too many sockets */
-							} /* too many tokens */
-						} /* data from client */
-					} /* i == sock */
-				} /* fd isset */
-		} /* infinate while */
+			    if (k > MAX_TOKENS - 1) {
+				(void)fprintf(stderr, "Server: too many "
+				    "tokens. Disconnected host %s, "
+				    "port %hd.\n",
+				    inet_ntoa(clientname.sin_addr),
+				    ntohs(clientname.sin_port));
+				write_to_client(new, "fail");
+				close(new);
+				FD_CLR(new, &active_fd_set);
+			    } else { 
+				for (l=0; sockets[k][l] != 0; l++)
+				    ;
+				if (l > MAX_CLUSTER - 1) {
+				    (void)fprintf(stderr, "Server: too many "
+					"sockets on token. Disconnected "
+					"host %s, port %hd.\n",
+					inet_ntoa(clientname.sin_addr),
+					ntohs(clientname.sin_port));
+				    write_to_client(new, "fail");
+				    close(new);
+				    FD_CLR(new, &active_fd_set);
+				} else {
+				    sockets[k][l] = new;
+				    tokens[k] = key;
+				    sizes[k] = atoi(buf);
+				    connections[k] += 1;
+				    if (connections[k] == sizes[k]) {
+					l = connections[k];
+					for (m=0; m < l; m++) {
+					    write_to_client(sockets[k][m],
+						"passed");
+					    close(sockets[k][m]);
+					    FD_CLR (sockets[k][m], 
+						&active_fd_set);
+					    sockets[k][m] = 0;
+					    tokens[k] = NULL;
+					    sizes[k] = 0;
+					    connections[k] = 0;
+					} /* for */
+				    } /* connections == sizes */
+				} /* too many sockets */
+			    } /* too many tokens */
+			} /* data from client */
+		    } /* i == sock */
+		} /* fd isset */
+	} /* infinate while */
 #ifndef DEBUG
-	default:
-		exit(EXIT_SUCCESS);
-		break;
-	} /* switch */
+    default:
+	exit(EXIT_SUCCESS);
+	break;
+    } /* switch */
 #endif
 }
 
 /*ARGSUSED*/
 void
-log_bailout(line) 
-	int line;
+_log_bailout(int line, char *file) 
 {
-	if (debug)
-		syslog(LOG_CRIT, "%s: Failed on line %d: %m %d", progname, line,
-			errno);
-	else
-		syslog(LOG_CRIT, "%s: Internal error, aborting: %m", progname);
+    syslog(LOG_CRIT, "%s: Failed in %s on line %d: %m %d", progname, file,
+	line, errno);
 
-	_exit(EXIT_FAILURE);
+    _exit(EXIT_FAILURE);
 }
