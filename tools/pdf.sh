453efed3e3e67186e8d2ff443c085be06160bff7
garbled <garbled>
2006-01-25 18:51:51 +0000

    Fix a bug in pdf -m pointed out by Ernst Rohlicek, and touch up the manpage a bit.

--- tools/pdf.sh
+++ tools/pdf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: pdf.sh,v 1.7 2005/12/12 08:00:06 garbled Exp $
+# $Id: pdf.sh,v 1.8 2006/01/25 18:51:51 garbled Exp $
 args=`getopt lng:m:t:w:x: $*`
 if test $? != 0
 then
@@ -22,7 +22,7 @@ do
 		-x)
 			xarg=$2; shift; shift;;
 		-m)
-			marg="$2"; shift; shift;;
+			marg=$2; shift; shift;;
 		--)
 			shift; break;;
 	esac
@@ -36,6 +36,9 @@ fi
 if [ -n "$xarg" ]; then
 	dshargs=`echo "$dshargs -x $xarg"`
 fi
+if [ -z "$marg" ]; then
+	marg=0;
+fi
 
 (
 dsh $dshargs 'sh -c "if [ `uname` = "AIX" ]; then df -kI '$flag $*'; elif [ `uname` = "HP-UX" ]; then bdf '$flag $*'; elif [ `uname` = "Linux" ]; then df -Pk '$flag $*'; else df -k '$flag $*'; fi"'
@@ -43,7 +46,7 @@ dsh $dshargs 'sh -c "if [ `uname` = "AIX" ]; then df -kI '$flag $*'; elif [ `una
 echo 'Node      Filesystem            1K-Blks     Used    Avail  Cap Mounted On'
 while read node fs blocks used avail capacity mount; do \
 	capacity=`echo "$capacity" | sed -e 's/\%//'`;
-	if [ "$capacity" -gt "$marg" -a 'test -n "$marg"' ]; then \
+	if [ $capacity -gt $marg ]; then \
 		printf "%-8s: %-19.19s %9d%9d%9d %3.3s%% %-17.17s\n" $node $fs $blocks $used $avail $capacity $mount; \
 	fi \
 done
